aef0d1918bc96b168d06883c864cae205cdb9e58
Stacey Boyer <opencatsdocs@gmail.com>
2017-09-19 19:46:34 -0400

    Updated Ubuntu 14.04 script with Travis build

--- Ubuntu14.04-OpenCATS-Install.sh
+++ Ubuntu14.04-OpenCATS-Install.sh
@@ -4,10 +4,10 @@
 
 
 export DEBIAN_FRONTEND=noninteractive
-apt-get update
+apt-get updatecd /var/www/html
 add-apt-repository -y ppa:ondrej/php
 apt-get update
-apt-get install -y mariadb-server mariadb-client apache2 php5.6 php5.6-soap php5.6-ldap php5.6-mysql php5.6-gd php5.6-curl php5.6-cli php5.6-xml php5.6-mbstring php5.6-zip curl git antiword poppler-utils html2text unrtf
+apt-get install -y unzip mariadb-server mariadb-client apache2 php5.6 php5.6-soap php5.6-ldap php5.6-mysql php5.6-gd php5.6-curl php5.6-cli php5.6-xml php5.6-mbstring php5.6-zip antiword poppler-utils html2text unrtf
 
 # Set up database
 mysql -u root --execute="CREATE DATABASE cats_dev;"
@@ -16,20 +16,15 @@ mysql -u root --execute="GRANT ALL PRIVILEGES ON cats_dev.* TO 'cats'@'localhost
 
 # Download OpenCATS
 cd /var/www/html
-wget https://github.com/opencats/OpenCATS/archive/0.9.3-3.tar.gz
-tar -xvzf 0.9.3-3.tar.gz
-mv OpenCATS-0.9.3-3 opencats
-cd /var/www/html/opencats
+wget https://github.com/opencats/OpenCATS/releases/download/0.9.4/opencats-0.9.4-full.zip
+unzip opencats-0.9.4-full.zip
+mv /var/www/html/home/travis/build/opencats/OpenCATS opencats
+rm -Rf /var/www/html/home /var/www/html/opencats/INSTALL_BLOCK
 
-# Install composer
-curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
-
-# Install OpenCATS composer dependancies
-composer install
-cd ..
 
 # Set file and folder permissions
-chown www-data:www-data -R opencats && chmod -R 770 opencats/attachments opencats/upload
+chown www-data:www-data -R opencats 
+chmod -R 770 opencats/attachments opencats/upload
 
 # Restart Apache to load new config
 service apache2 restart
